62cf8b5017a64bab0bbfa12d7c44f16a1b3a1723
Junior Silva <150052476+juniorlds98@users.noreply.github.com>
2025-04-16 21:23:09 -0300

    Add files via upload

---
+++ src/TelaConfirmacao.jsx
@@ -0,0 +1,14 @@
+import imgUrl from './assets/img/Hospital-Infantil-Sabará.png'
+
+function TelaConfirmacao() {
+  return (
+    <>
+      <div>
+        <p>Hospital Sabará</p>
+        <img src={imgUrl} alt="Hospital Infantil Sabará" />
+      </div>
+    </>
+  )
+}
+
+export default TelaConfirmacao
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import TelaConfirmacao from './TelaConfirmacao';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <TelaConfirmacao />
+  </StrictMode>
+);
