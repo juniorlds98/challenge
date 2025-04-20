40264bc678bba8d95d15ab2aa5b0d844ace42200
Guilherme <guiroleite@gmail.com>
2025-04-20 16:39:20 -0300

    Feature/Triagem Update Page

--- ChallengeFront/src/main.tsx
+++ ChallengeFront/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import Index from './index.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     < Index/>
